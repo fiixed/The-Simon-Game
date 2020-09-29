e25352ef97da2201febab41b83cce5803ca1f785
fiixed <fiixed@gmail.com>
2020-09-29 16:29:53 -0500

    game logic

---
+++ game.js
@@ -0,0 +1,32 @@
+/*jshint esversion: 6 */
+
+let buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+
+$(".btn").on("click", function () {
+  let userChosenColour = this.attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+});
+
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $(`.${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio(`sounds/${name}.mp3`);
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $(`#${currentColour}`).addClass("pressed");
+  setTimeout(function () {
+    $(`#${currentColour}`).removeClass("pressed");
+  }, 100);
+}
