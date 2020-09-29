d89e703bdab407f33c5f38b2cf45c961788cca21
fiixed <fiixed@gmail.com>
2020-09-29 16:39:48 -0500

    more game logic

--- game.js
+++ game.js
@@ -3,6 +3,8 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+let started = false;
+let level = 0;
 
 $(".btn").on("click", function () {
   let userChosenColour = this.attr("id");
@@ -10,7 +12,17 @@ $(".btn").on("click", function () {
   playSound(userChosenColour);
 });
 
+$(document).on("keypress", function () {
+  if (!started) {
+    $("#level-title").text(`Level ${level}`);
+    nextSequence();
+    started = true;
+  }
+});
+
 function nextSequence() {
+  level++;
+  $("#level-title").text(`Level ${level}`);
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColors[randomNumber];
   gamePattern.push(randomChosenColour);
